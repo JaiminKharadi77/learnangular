0b6b65be90e329ea38b98cb85356d11242753bd4
Jaimin1999 <learnwithjaimin@gmail.com>
2025-01-17 23:45:03 +0530

    Angular Tutorial - 5 - Interpolation

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  imports: [],
-  templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+  template: `
+    <div>
+      <h2>Welcome {{ name }}</h2>
+      <!-- <h2>{{ calculateSum(2, 2) }}</h2> -->
+    </div>
+  `,
+  styles: [],
 })
 export class TestComponent {
+  name: string = 'Guest';
 
+  
+
+  calculateSum(a: number, b: number): number {
+    return a + b;
+  }
 }
