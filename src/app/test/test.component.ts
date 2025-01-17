dfa1d6d6ad16436e2d90961aea2c0d7e250900b9
Jaimin1999 <learnwithjaimin@gmail.com>
2025-01-17 23:51:13 +0530

    Angular Tutorial - 6 - Property Binding

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
     <div>
       <h2>Welcome {{ name }}</h2>
       <!-- <h2>{{ calculateSum(2, 2) }}</h2> -->
+
+      <input
+        [id]="myId"
+        bind-disabled="isDisabled"
+        type="text"
+        value="Jaimin"
+      />
+      <input
+        id="{{ myId }}"
+        [disabled]="isDisabled"
+        type="text"
+        value="Jaimin"
+      />
     </div>
   `,
   styles: [],
@@ -13,7 +26,9 @@ import { Component } from '@angular/core';
 export class TestComponent {
   name: string = 'Guest';
 
-  
+  public myId: string = 'testId';
+
+  public isDisabled: boolean = false;
 
   calculateSum(a: number, b: number): number {
     return a + b;
